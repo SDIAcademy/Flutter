066c8684f9baf57a94fb01e68b12a956b8a59b84
Zayn <zaynjarvis@gmail.com>
2018-05-19 07:16:41 +0800

    sliver

--- myfirebase/lib/main.dart
+++ myfirebase/lib/main.dart
@@ -1,171 +1,118 @@
-// Copyright 2017, the Flutter project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:io' show Platform;
+import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-Future<void> main() async {
-  final FirebaseApp app = await FirebaseApp.configure(
-    name: 'DB',
-    options: Platform.isIOS
-        ? const FirebaseOptions(
-            googleAppID: '1:297855924061:ios:c6de2b69b03a5be8', // FAKE
-            gcmSenderID: '297855924061',
-            databaseURL: 'https://flutterfire-cd2f7.firebaseio.com',
-          )
-        : const FirebaseOptions(
-            googleAppID: '1:1081776211548:android:a8a6f5983ca93d9b',
-            apiKey: 'AIzaSyDET4eaCfWJ-VgWMKMfNVCCeazY_Bk9bIg',
-            databaseURL: 'https://myflutter-34cd5.firebaseio.com',
-          ),
-  );
-  runApp(new MaterialApp(
-    title: 'Flutter Database Example',
-    home: new MyHomePage(app: app),
-  ));
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = new GoogleSignIn();
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Firebase Auth Demo',
+      home: new MyHomePage(title: 'Firebase Auth Demo'),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({this.app});
-  final FirebaseApp app;
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter;
-  DatabaseReference _counterRef;
-  DatabaseReference _messagesRef;
-  StreamSubscription<Event> _counterSubscription;
-  StreamSubscription<Event> _messagesSubscription;
-  bool _anchorToBottom = false;
+  Future<String> _message = new Future<String>.value('');
 
-  String _kTestKey = 'Hello';
-  String _kTestValue = 'world!';
-  DatabaseError _error;
-
-  @override
-  void initState() {
-    super.initState();
-    // Demonstrates configuring to the database using a file
-    _counterRef = FirebaseDatabase.instance.reference().child('counter');
-    // Demonstrates configuring the database directly
-    final FirebaseDatabase database = new FirebaseDatabase(app: widget.app);
-    _messagesRef = database.reference().child('messages');
-    database.reference().child('counter').once().then((DataSnapshot snapshot) {
-      print('Connected to second database and read ${snapshot.value}');
-    });
-    database.setPersistenceEnabled(true);
-    database.setPersistenceCacheSizeBytes(10000000);
-    _counterRef.keepSynced(true);
-    _counterSubscription = _counterRef.onValue.listen((Event event) {
-      setState(() {
-        _error = null;
-        _counter = event.snapshot.value ?? 0;
-      });
-    }, onError: (Object o) {
-      final DatabaseError error = o;
-      setState(() {
-        _error = error;
-      });
-    });
-    _messagesSubscription =
-        _messagesRef.limitToLast(10).onChildAdded.listen((Event event) {
-      print('Child added: ${event.snapshot.value}');
-    }, onError: (Object o) {
-      final DatabaseError error = o;
-      print('Error: ${error.code} ${error.message}');
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _messagesSubscription.cancel();
-    _counterSubscription.cancel();
-  }
-
-  Future<Null> _increment() async {
-    // Increment counter in transaction.
-    final TransactionResult transactionResult =
-        await _counterRef.runTransaction((MutableData mutableData) async {
-      mutableData.value = (mutableData.value ?? 0) + 1;
-      return mutableData;
-    });
-
-    if (transactionResult.committed) {
-      _messagesRef.push().set(<String, String>{
-        _kTestKey: '$_kTestValue ${transactionResult.dataSnapshot.value}'
-      });
-    } else {
-      print('Transaction not committed.');
-      if (transactionResult.error != null) {
-        print(transactionResult.error.message);
-      }
+  Future<String> _testSignInAnonymously() async {
+    final FirebaseUser user = await _auth.signInAnonymously();
+    assert(user != null);
+    assert(user.isAnonymous);
+    assert(!user.isEmailVerified);
+    assert(await user.getIdToken() != null);
+    if (Platform.isIOS) {
+      // Anonymous auth doesn't show up as a provider on iOS
+      assert(user.providerData.isEmpty);
+    } else if (Platform.isAndroid) {
+      // Anonymous auth does show up as a provider on Android
+      assert(user.providerData.length == 1);
+      assert(user.providerData[0].providerId == 'firebase');
+      assert(user.providerData[0].uid != null);
+      assert(user.providerData[0].displayName == null);
+      assert(user.providerData[0].photoUrl == null);
+      assert(user.providerData[0].email == null);
     }
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+
+    return 'signInAnonymously succeeded: $user';
+  }
+
+  Future<String> _testSignInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final FirebaseUser user = await _auth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+
+    return 'signInWithGoogle succeeded: $user';
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: const Text('Flutter Database Example'),
+        title: new Text(widget.title),
       ),
       body: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          new Flexible(
-            child: new Center(
-              child: _error == null
-                  ? new Text(
-                      'Button tapped $_counter time${ _counter == 1 ? '' : 's' }.\n\n'
-                          'This includes all devices, ever.',
-                    )
-                  : new Text(
-                      'Error retrieving button tap count:\n${_error.message}',
-                    ),
-            ),
-          ),
-          new ListTile(
-            leading: new Checkbox(
-              onChanged: (bool value) {
+          new MaterialButton(
+              child: const Text('Test signInAnonymously'),
+              onPressed: () {
                 setState(() {
-                  _anchorToBottom = value;
+                  _message = _testSignInAnonymously();
                 });
-              },
-              value: _anchorToBottom,
-            ),
-            title: const Text('Anchor to bottom'),
-          ),
-          new Flexible(
-            child: new FirebaseAnimatedList(
-              key: new ValueKey<bool>(_anchorToBottom),
-              query: _messagesRef,
-              reverse: _anchorToBottom,
-              sort: _anchorToBottom
-                  ? (DataSnapshot a, DataSnapshot b) => b.key.compareTo(a.key)
-                  : null,
-              itemBuilder: (BuildContext context, DataSnapshot snapshot,
-                  Animation<double> animation, int index) {
-                return new SizeTransition(
-                  sizeFactor: animation,
-                  child: new Text("$index: ${snapshot.value.toString()}"),
-                );
-              },
-            ),
-          ),
+              }),
+          new MaterialButton(
+              child: const Text('Test signInWithGoogle'),
+              onPressed: () {
+                setState(() {
+                  _message = _testSignInWithGoogle();
+                });
+              }),
+          new FutureBuilder<String>(
+              future: _message,
+              builder: (_, AsyncSnapshot<String> snapshot) {
+                return new Text(snapshot.data ?? '',
+                    style: const TextStyle(
+                        color: const Color.fromARGB(255, 0, 155, 0)));
+              }),
         ],
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _increment,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
